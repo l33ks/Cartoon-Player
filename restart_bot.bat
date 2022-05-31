1c3ea0407a878ff132e003ad06e2227e3fd450ac
l33ks <l33ks@l33ks.local>
2022-05-31 17:11:16 -0400

    start cmd

--- restart_bot.bat
+++ restart_bot.bat
@@ -1,1 +1,1 @@
-tasklist /v /fo csv | findstr /i "streamer_bot" > pid_temp && for /F "tokens=2 delims=," %%i in (pid_temp) do taskkill /f /t /pid %%~i && del pid_temp && start_bot.bat
+start cmd /c tasklist /v /fo csv | findstr /i "streamer_bot" > pid_temp && for /F "tokens=2 delims=," %%i in (pid_temp) do taskkill /f /t /pid %%~i && del pid_temp && start_bot.bat
